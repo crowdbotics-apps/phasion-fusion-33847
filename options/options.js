cd99cde59678859ddad37d25e12733aafb056d4a
Crowdbotics <team@crowdbotics.com>
2022-02-25 15:17:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "phasion_fusion_33847",
+  url: "https://phasion_fusion_33847.botics.co",
+  api: "https://phasion_fusion_33847.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
